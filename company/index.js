5d200223c9b4249859eb0858ada26c74b23aea2c
Arthur1asdf <arthurteng0819@gmail.com>
2024-05-04 18:26:01 -0400

    made it so the random button would change with the color of the background when clicking the random button

--- company/index.js
+++ company/index.js
@@ -15,10 +15,15 @@ function setColor(name)
 
 function randomColor()
 {
+    
     let red = Math.round(Math.random() * 255);
     let green = Math.round(Math.random() * 255);
     let blue = Math.round(Math.random() * 255);
     let color =  `rgb(${red}, ${green}, ${blue})`;
+    if(randomColorButton)
+    {
+        randomColorButton.style.backgroundColor = color;
+    }
     setColor(color);
 
 }
