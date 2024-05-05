3659be26a44fce1b9e0bbf35e883f4e57e4f32d7
Arthur1asdf <arthurteng0819@gmail.com>
2024-05-04 20:47:47 -0400

    added some images to use fixed typo in some fun buttons to press added a button that would change the background to a picrture and change hidden text to non hidden text and change it back to hidden once you press another button

--- company/index.js
+++ company/index.js
@@ -1,7 +1,8 @@
 const body = document.getElementsByTagName("body")[0];
 const randomColorButton = document.getElementById("random");
 let transitionTimer = null;
-function checkTransitionTimer(){
+function checkTransitionTimer()
+{
     if(transitionTimer != null)
     {
         clearTimeout(transitionTimer);
@@ -9,13 +10,14 @@ function checkTransitionTimer(){
 }
 function setColor(name)
 {
+    body.style.backgroundImage ='none';
+    jumpScare.style.display = 'none';
     checkTransitionTimer();
     body.style.backgroundColor = name;
 }
 
 function randomColor()
 {
-    
     let red = Math.round(Math.random() * 255);
     let green = Math.round(Math.random() * 255);
     let blue = Math.round(Math.random() * 255);
@@ -26,7 +28,6 @@ function randomColor()
     }
     setColor(color);
 }
-
 function rgbButton()
 {
     let red = Math.round(Math.random() * 255);
@@ -35,4 +36,13 @@ function rgbButton()
     let color =  `rgb(${red}, ${green}, ${blue})`;
     setColor(color);
     transitionTimer = setTimeout(rgbButton, 2000);
+}
+var jumpScare = document.getElementById("jumpScare");
+function imageButton()
+{
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundPosition = "center top";
+    body.style.backgroundImage ="url('/company/imagesForButton/fnafbeat.jpg')";
+    jumpScare.style.display = "block";
+    
 }
